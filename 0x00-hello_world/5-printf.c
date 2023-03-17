6c068afcb151b529be8f8455f1d53ef3cba3b2f7
gabbrenn <gabvladimirbrenn@gmail.com>
2023-03-17 15:20:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stio.h>
+/**
+* main - Aprogram that prints a line puts function
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
