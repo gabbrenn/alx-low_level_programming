c99f62e3aaa6b288db16831282c31f42f41bddef
gabbrenn <gabvladimirbrenn@gmail.com>
2023-03-17 14:04:46 -0700

    /**  * main - Aprogram that prints a line puts function  * Return:0 (Success) clear ihindutse carine hhhhhhhhhhhhhh clear :q :wq ls cd '' cg .. cd .. git add . git push git commit -m /**

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-
+/**
+ * main - Aprogram that prints a line puts function
+ * Return:0 (Success)
+*/
 int main(){
 
 puts("/"Programming is like building a multilingual puzzle");
 
-return(0)
+return(0);
 
 
 
