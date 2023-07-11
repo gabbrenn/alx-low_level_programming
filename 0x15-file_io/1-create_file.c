71fb685b2461b7c9dc33bf4d768dc68cc3bda3f9
gabbrenn <gabvladimirbrenn@gmail.com>
2023-07-11 11:52:46 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, i;
+  int fd, wt, l = 0;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return -1;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[l];)
+			len++;
+	}
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-            ;
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wt = write(fd, text_content, l);
 
-        bytes_written = write(fd, text_content, i);
-        if (bytes_written == -1 || bytes_written != i)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	if (fd == -1 || wt == -1)
+		return (-1);
 
-    close(fd);
-    return 1;
+	close(fd);
+
+	return (1);
 }
-
