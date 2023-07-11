291486449dbb01d7fff7e99d18c4421bd51860a8
gabbrenn <gabvladimirbrenn@gmail.com>
2023-07-11 12:28:48 -0700

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,35 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
+ * append_text_to_file - Appends text at end of a file.
+ * @filename: A pointer of name of the file.
+ * @text_content: The string to be added to the end of the file.
  *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
+ * Return: When the function fails or filename is NULL - -1.
+ *         When the file does not exist the user lacks write permissions - -1.
  *         Else - 1.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written, i;
+	int fd, gw, i = 0;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return -1;
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i] != '\0'; i++)
-            ;
+	fd = open(filename, O_WRONLY | O_APPEND);
+	gw = write(fd, text_content, i);
 
-        bytes_written = write(fd, text_content, i);
-        if (bytes_written == -1 || bytes_written != i)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	if (fd == -1 || gw == -1)
+		return (-1);
 
-    close(fd);
-    return 1;
+	close(fd);
+
+	return (1);
 }
 
